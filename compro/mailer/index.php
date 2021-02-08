37466ca63ba43b57669f4ea102863b6b57ba4124
Qahtan <qahtansaidmartak@gmail.com>
2021-02-09 00:45:00 +0700

    remove pass

--- compro/mailer/index.php
+++ compro/mailer/index.php
@@ -24,8 +24,8 @@ try {
     $mail->isSMTP(); // Send using SMTP
     $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
     $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = 'endierdi79@gmail.com'; // SMTP username
-    $mail->Password = '7248267242Z'; // SMTP password
+    $mail->Username = ''; // SMTP username
+    $mail->Password = ''; // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
